85f08ea63ff51c3091a1d7d8901d1dd8a5b3088f
Sukantasai <106627772+Sukantasai@users.noreply.github.com>
2025-06-30 15:36:49 +0000

    conversionoperator is done

---
+++ 01_basics/03_conversionOperator.js
@@ -0,0 +1,40 @@
+let score = "Sukanta";
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+let isLoggedIn = "Sukanta"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+let someNumber = "123abc";
+let stringNumber = String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log("2" + 2);
+// console.log(2-2);
+// console.log(2**2);
+// console.log(2%3);
+
+let str1 ="hello";
+let str2 = "Sukanta";
+let str3 = str1 + str2
+// console.log(str3);
+
+let str4 = "2" + 2;
+// console.log(str4);
+// console.log((3+4)*5%3);
+
+let num1 , num2, num3
+num1 = num2 = num3 = 2+2;
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
