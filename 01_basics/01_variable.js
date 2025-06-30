9ef1cafe0d62176b1c0c84a878ef213748f940aa
Sukantasai <106627772+Sukantasai@users.noreply.github.com>
2025-06-30 11:47:55 +0000

    datatype

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,12 @@
+const accountId = 1234567890;
+let accountemail = "saisukanta8@gmail.com";
+var accountPassword = "12345";
+accountCity = "Kolkata";
+let accountState;
+// accountId = 234;
+// console.log(accountId);
+accountemail = "ss@gl.com";
+accountPassword = "54321";
+accountCity = "Delhi";
+
+console.table([accountId, accountemail, accountPassword, accountCity, accountState]);
