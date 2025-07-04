54efd82854e92503c8e59c13dfe7a3b621672441
Sukantasai <106627772+Sukantasai@users.noreply.github.com>
2025-07-04 08:39:54 +0530

    Add files via upload

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,23 @@
+const name = "Sukanta"
+const repoCount = 50
+// console.log(name + repoCount + "Value");
+// console.log('hello my name is ${name} and my repo count is ${repoCount}');
+const gameName = new String('sukanta-ss-com')
+// console.log(gameName[5]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+const newString = gameName.substring(0,4)
+// console.log(newString);
+const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+const newStringOne = "  sukanta  "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url ="https:sukanta.com/sukanta%20sai"
+console.log(url.replace('%20', '-'))
+console.log(url.includes('sundar'))
+console.log(gameName.split('-'));
