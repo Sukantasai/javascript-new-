d18ba5c9f2bea8059a1b715000f2607ca43605ee
Sukantasai <106627772+Sukantasai@users.noreply.github.com>
2025-07-04 11:18:56 +0530

    Add files via upload

---
+++ 01_basics/07_dates.js
@@ -0,0 +1,25 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toJSON());
+
+// let myCreatedDate = new Date(2023 , 1 , 23)
+// let myCreatedDate = new Date(2023 , 1 , 23 , 5 ,10)
+let myCreatedDate = new Date("01-04-2025")
+
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+// console.log(Math.floor(Date.now()/1000));
+
+// console.log(myCreatedDate.toDateString())
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth() + 1)
+console.log(newDate.getDay());
